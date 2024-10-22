82a26135c6f0b8ece915cf8b65d1f60814e75abf
ClumsyLulz <86472964+SleepTheGod@users.noreply.github.com>
2024-10-22 10:40:15 -0400

    Update capture_proxy.py

--- capture_proxy.py
+++ capture_proxy.py
@@ -1,1 +1,29 @@
+from mitmproxy import http
+from mitmproxy.tools.dump import DumpMaster
+from mitmproxy.options import Options
+import requests
 
+class Interceptor:
+    def request(self, flow: http.HTTPFlow):
+        print(f"Intercepted request: {flow.request.method} {flow.request.url}")
+        headers = dict(flow.request.headers)
+        data = {
+            'url': flow.request.url,
+            'method': flow.request.method,
+            'headers': headers,
+            'body': flow.request.content.decode('utf-8') if flow.request.content else ''
+        }
+        requests.post('http://localhost:5000/capture', json=data)
+
+def start_proxy():
+    options = Options(listen_host='0.0.0.0', listen_port=8080, ssl_insecure=True)
+    master = DumpMaster(options)
+    addon = Interceptor()
+    master.addons.add(addon)
+    try:
+        master.run()
+    except KeyboardInterrupt:
+        master.shutdown()
+
+if __name__ == "__main__":
+    start_proxy()
